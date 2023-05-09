603a450cf231e65859a91769037f355ad7c7f350
OlenkaPasko <lena_nechipurenko@ukr.net>
2023-05-09 08:53:02 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,8 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+export class App {
+  //початковий стан
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+}
